bc2e8bfe70aebe75057c2aeca2580663a397ef41
Aquivion <aquivion@gmail.com>
2024-06-20 23:34:59 +0200

    Fetch data in server component UsersPage

--- issue-tracker/app/users/page.tsx
+++ issue-tracker/app/users/page.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
 
-const UsersPages = () => {
-  return (
-    <div>UsersPages</div>
-  )
+interface User {
+	id: number
+	name: string
 }
 
-export default UsersPages
+const UsersPage = async () => {
+	const res = await fetch('https://jsonplaceholder.typicode.com/users')
+	const users: User[] = await res.json()
+	return (
+		<>
+			<h1>Users</h1>
+			<ul>
+				{users.map((user) => (
+					<li key={user.id}>{user.name}</li>
+				))}
+			</ul>
+		</>
+	)
+}
+
+export default UsersPage
